42749dd75bbf5f80f9d400207ac3c260c858b397
Jonathan <jonathannasc2@gmail.com>
2024-10-05 14:19:19 -0300

    Optimize code structure

--- desafio2.js
+++ desafio2.js
@@ -1,28 +1,22 @@
 function calculateLevel(wins, losses) {
-
     const winBalance = wins - losses;
-    let level;
+
+    const levels = [
+        { threshold: 10, level: 'Ferro' },
+        { threshold: 21, level: 'Bronze' },
+        { threshold: 51, level: 'Prata' },
+        { threshold: 81, level: 'Ouro' },
+        { threshold: 91, level: 'Diamante' },
+        { threshold: 101, level: 'Lendário' },
+        { threshold: Infinity, level: 'Imortal' }
+    ];
 
 
-    if (wins < 10) {
-        level = 'Ferro';
-    } else if (wins >= 11 && wins <= 20) {
-        level = 'Bronze';
-    } else if (wins >= 21 && wins <= 50) {
-        level = 'Prata';
-    } else if (wins >= 51 && wins <= 80) {
-        level = 'Ouro';
-    } else if (wins >= 81 && wins <= 90) {
-        level = 'Diamante';
-    } else if (wins >= 91 && wins <= 100) {
-        level = 'Lendário';
-    } else if (wins >= 101) {
-        level = 'Imortal';
-    }
+    const level = levels.find(({ threshold }) => wins < threshold).level;
+
     return `O Herói tem de saldo de **${winBalance}** está no nível de **${level}**`;
 }
 
-
-console.log(calculateLevel(85, 10)); 
-console.log(calculateLevel(95, 5));   
-console.log(calculateLevel(105, 0));  
+console.log(calculateLevel(85, 10));  
+console.log(calculateLevel(95, 5));  
+console.log(calculateLevel(105, 0));
